f6391b9982053c4cfc15ce315fb2eb36c97d2dc2
Christopher Moore <christopher.e.moore@gmail.com>
2017-03-19 12:12:25 +0100

    more stuff

--- deck.lua
+++ deck.lua
@@ -1,16 +1,18 @@
 local class = require 'ext.class'
 local table = require 'ext.table'
 local Stack = require 'cards.stack'
-local Card = require 'cards.card'
+
 local Deck = class(Stack)
+
 function Deck:init()
 	Deck.super.init(self)
 	for value=1,13 do
 		for suit=1,4 do
-			self.cards:insert(Card{value=value, suit=suit})
+			self.cards:insert(self.Card{value=value, suit=suit})
 		end
 	end
 end
+
 -- in-place, returns self.
 -- this invalidates self.cards.  if that matters then i could do it twice to restore the same table...
 function Deck:shuffle()
@@ -21,7 +23,9 @@ function Deck:shuffle()
 	self.cards = cards
 	return self
 end
+
 function Deck:takeTop()
 	return self.cards:remove()
 end
+
 return Deck
--- stack.lua
+++ stack.lua
@@ -1,22 +1,29 @@
 local class = require 'ext.class'
 local table = require 'ext.table'
-local Card = require 'cards.card' 
+
 local Stack = class()
+
+Stack.Card = require 'cards.card'
+
 function Stack:init(args)
 	self.cards = table(args and args.cards or {})
 end
+
 function Stack:last(fromLast)	-- fromLast == 1 <=> last card, fromLast == 2 <=> 2nd to last card
 	return self.cards[#self.cards+1-(fromLast or 1)]
 end
+
 function Stack:__tostring()
 	return self.cards:map(tostring):concat' '
 end
+
 function Stack:clone()
 	local clone = getmetatable(self)()
 	clone.cards = table()
 	for i=1,#self.cards do
-		clone.cards[i] = Card(self.cards[i])
+		clone.cards[i] = self.Card(self.cards[i])
 	end
 	return clone
 end
+
 return Stack
